9420c9d79be8f46be45b1375a4467c6d49285247
d.arishev <sividmitry@gmail.com>
2022-03-15 16:57:46 +0300

    404 or find name allias clear code

--- slim-example/public/index.php
+++ slim-example/public/index.php
@@ -4,43 +4,30 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
-
-
-
-
 $app = AppFactory::create();
 
-// Метод `setName` задаёт имя маршрута. Затем это имя используется для формирования нужного адреса. Ниже пример того как это сделать:
-
-
 $app->get('/404', function ($request, $response) {
-
     return $response->withStatus(404)->withHeader('Content-Type', 'text/h2tsssssml')  ;
-
-
 })->setName('notfound');
 
 $router = $app->getRouteCollector()->getRouteParser();
 
-
 $app->get( '/', function ($request, $response) use ($router) {
     return $response->withRedirect( $router->urlFor('notfound'));
-
 });
 
-$app->get( '/name/{name}', function ($request, $response, $args ) use($router) {
-        $name = $args['name'];
-        $array = explode("\n", file_get_contents('names'));
-        $search = array_search($name, $array);
-       if ($name == $array[$search]){
+    $app->get( '/name/{name}', function ($request, $response, $args ) use($router) {
 
-           return $response->write($name . ' Найдено');
-       }else{
-        return $response->withRedirect( $router->urlFor('notfound'));
-       }
+                $name = $args['name'];
+                $array = explode("\n", file_get_contents('names'));
+                $search = array_search($name, $array);
 
-
-});
+           if ($name == $array[$search]){
+               return $response->write($name . ' Найдено');
+           }else{
+               return $response->withRedirect( $router->urlFor('notfound'));
+           }
+    });
 
 
 $app->run();
