8c9fb591763635497bd969c6c149a34f060b6f45
d.arishev <sividmitry@gmail.com>
2022-03-18 17:11:30 +0300

    myTasks Arrays

--- lessonsFromNotion/getChunked.php
+++ lessonsFromNotion/getChunked.php
@@ -1,10 +1,11 @@
 <?php
 $array = [[10, 11, 24], [10, 13, 14, 18, 24, 30]];
 
-    function getChunked($array,$lenght)
-    {
+    function getChunked($array,$lenght){
+
         $array = $array[[1,2,3,4]];
         var_dump($array);
     }
+
 
     getChunked($array,0);
